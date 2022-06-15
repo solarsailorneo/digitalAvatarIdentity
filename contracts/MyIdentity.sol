6b4a4464ed71ae9ccec790db96ebe03c152faff3
Solar Sailor <solarsailorneo@protonmail.com>
2022-06-15 17:21:17 -0400

    Parsing through higher level contract to achieve base erc725 (not done))

--- contracts/MyIdentity.sol
+++ contracts/MyIdentity.sol
@@ -3,6 +3,31 @@ pragma solidity ^0.8.4;
 
 import "@erc725/smart-contracts/contracts/ERC725.sol";
 
+/**
+* @author SolarSailor
+* @thanks AMT 
+*/
+
+struct identity {
+    string name; // Name of the person
+    string linktree; // linktree
+    string photoHash; // IPFS hash of photo
+    }
+
 contract MyIdentity is ERC725 {
+    // address creatorAddress;
+    mapping(string => identity) identity_id;
+    mapping(string => bool) public emailId;
+    mapping(uint256 => bool) public contactNum;
+
+    // constructor() ERC725(msg.sender) public {
+    
+    //     creatorAddress = msg.sender;  //Person who deploys this contract will be the owner
+    
+    // }
+    
+
+
+
 
 }
